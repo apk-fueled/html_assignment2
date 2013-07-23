cb2b5a33ca99f7b5dbb10e1fd6ab697ed83ea559
apk-fueled <apk@fueled.co>
2013-07-23 17:27:08 +0530

    html assignment2

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+var Site = {
+	init : function() {
+		this.tabs();
+	},
+
+	tabs : function() {
+		var tabs = $('#secondary > .tabs'),
+			uls = tabs.find('> ul'),
+			tabHeadings = tabs.prev('#tabHeadings');
+
+
+			tabs.find('h4').remove();
+
+			uls.not(':first').hide();
+
+
+		// listen for heading clicks
+		tabHeadings
+			.delegate('li', 'click', function(e) {
+				var li = $(this),
+					hash;
+
+					// change the selected class to the selected one
+					li
+						.siblings()
+							.removeClass('selected')
+						.end()
+						.addClass('selected');
+
+					// grab the hash of anchor
+					hash = li.children('a').attr('href');
+
+					// show correspondong sections
+					uls.hide().filter(hash).show();
+			});
+	}
+}
+
+Site.init();
